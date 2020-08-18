66424bf3b329f0c0a313252890aca7d4b0cb5f7c
ravigavde <nightr714@gmail.com>
2020-08-18 11:34:35 +0530

    Calculator file added
    Addition feature added

---
+++ Calc.java
@@ -0,0 +1,18 @@
+public class Calc
+{
+    int add(int a, int b)
+    {
+        return(a+b);
+    }
+    public static void main(String arg[]) 
+    {
+        int a = 0, int b = 0 ;
+        Scanner sc = new Scanner(System.in); 
+        Calc c = new Calc();
+        System.out.println("Enter first number :-");
+        a = sc.nextInt();
+        System.out.println("Enter Second number :-");
+        b = sc.nextInt();
+        System.out.println("Addition is :- "+c.add(a,b));
+    }
+}
